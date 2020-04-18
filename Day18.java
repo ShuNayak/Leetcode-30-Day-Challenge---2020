48482f28f63da881eb3da689fd271854a8a863fc
ShuNayak <shubhadafall2019@gmail.com>
2020-04-18 10:31:31 -0700

    Minimum Path Sum, Java

---
+++ Day18.java
@@ -0,0 +1,34 @@
+package com.ThirtyDayLeetChallenge.letsdotit;
+
+public class Day18 {
+
+	 public static int minPathSum(int[][] grid) {
+	        
+	        if(null == grid || grid.length ==0) return 0;
+	        
+	        //creating a DP array
+	        int[][] dp = new int[grid.length][grid[0].length];
+	        for(int i=0;i<grid.length;i++){
+	            for(int j=0;j<grid[i].length;j++){
+	                dp[i][j] += grid[i][j];
+	                if(i>0 && j>0){
+	                    dp[i][j]+= Math.min(dp[i-1][j], dp[i][j-1]);
+	                }
+	               else if(i>0){
+	                    dp[i][j]+= dp[i-1][j];
+	                }
+	                else if(j>0){
+	                    dp[i][j]+= dp[i][j-1];
+	                }
+	            }
+	    }
+	        
+	        return dp[grid.length-1][grid[0].length-1];
+	        
+	    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
